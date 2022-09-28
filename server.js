10f3ce27faefbba3b2b0e64ab00db0cbe291d4f9
Deepak Singh <104123364+Deepak0000000@users.noreply.github.com>
2022-09-28 17:45:58 +0530

    initial

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express(); 
+const ejs = require("ejs")
+const path =require("path")
+const expressLayouts = require("express-ejs-layouts")
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res)=>{
+    res.render("home")
+})
+
+// set template engne 
+
+app.use(expressLayouts)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
+
+
+app.listen(PORT, ()=>{
+    console.log(`listening to the port ${PORT}`)
+
+})
